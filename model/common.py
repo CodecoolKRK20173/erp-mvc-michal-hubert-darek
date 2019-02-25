8b1c8fe5eea27a005f75ab75e72e4d01bcbe6124
Dariusz Serwaczak "Innewit <serwaczak.dariusz@gmail.com>
2019-02-25 11:23:44 +0100

    Added generate_random function

--- model/common.py
+++ model/common.py
@@ -18,7 +18,25 @@ def generate_random(table):
     """
 
     generated = ''
+    lower_letter = lambda: chr(random.randint(ord('a'), ord('z')))
+    upper_letter = lambda: chr(random.randint(ord('A'), ord('Z')))
+    number = lambda: random.randint(0,9)
+    
+    def special():
+        exception = ("+", "-", "=", ";", ":", "'", '"', ",", ".") + tuple(map(str, range(0, 10)))
+        generated_special_chr = chr(random.randint(ord('!'), ord('@')))
+        while(generated_special_chr in exception):
+            generated_special_chr = chr(random.randint(ord('!'), ord('@')))# witouth exception tuple characters
+        return generated_special_chr
 
-    # your code
+    generated = f"{lower_letter()}{upper_letter()}{number()}{number()}{upper_letter()}{lower_letter()}{special()}{special()}"
 
-    return generated
+    if generated not in table: 
+        return generated# Example kH94Ju#&
+    else: 
+        try:
+            generate_random(table)
+        except RecursionError:
+            print("Stack overflow error!")
+
+print(generate_random(["kH94Ju#&"]))
