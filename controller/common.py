a93924317095289f273554a9110158e14fe6a739
Dariusz Serwaczak "Innewit <serwaczak.dariusz@gmail.com>
2019-02-25 12:27:53 +0100

    Added quicksort function

--- controller/common.py
+++ controller/common.py
@@ -1,3 +1,16 @@
 """ Common functions for controllers
 implement commonly used functions here
 """
+
+def quicksort(table_to_sort, ascending_order = True):
+    if not table_to_sort: return []
+    if len(table_to_sort) == 1: return table_to_sort
+
+    pivot_element = table_to_sort.pop(0)
+    lower_elements = [element for element in table_to_sort if element <= pivot_element]
+    higher_elements = [element for element in table_to_sort if element > pivot_element]
+
+    if not ascending_order: 
+        lower_elements, higher_elements = higher_elements, lower_elements
+
+    return quicksort(lower_elements, ascending_order) + [pivot_element] + quicksort(higher_elements, ascending_order)
