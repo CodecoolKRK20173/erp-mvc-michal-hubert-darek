06b9962346b5987cb5cd6b76dce8c3517dd62a59
Dariusz Serwaczak "Innewit <serwaczak.dariusz@gmail.com>
2019-02-26 13:31:04 +0100

    Refactor model and update common.py

--- model/common.py
+++ model/common.py
@@ -4,6 +4,107 @@ implement commonly used functions here
 import random
 
 
+def add(table, record):
+    """
+    Add new record to table
+
+    Args:
+        table (list): table to add new record to
+        record (list): new record
+
+    Returns:
+        list: Table with a new record
+    
+    >>> add([["record", "one"]], ["new", "record"])
+    [['record', 'one'], ['new', 'record']]
+
+    >>> add([], ["new", "record"])
+    [['new', 'record']]
+    
+    >>> add([], ["new", "record"])
+    [['new', 'record']]
+    
+    >>> add([], [])
+    [[]]
+    """
+
+    output_table = table.copy()
+    output_table.append(record)
+
+    return output_table
+
+
+def remove(table, id_):
+    """
+    Remove a record with a given id from the table.
+
+    Args:
+        table (list): table to remove a record from
+        id_ (str): id of a record to be removed
+
+    Returns:
+        list: Table without specified record.
+        
+    >>> remove([["record", "one"], ["record", "two"]], 1)
+    [['record', 'one']]
+    >>> remove([["record", "one"]], 0)
+    []
+    >>> remove([[]], 0)
+    []
+    >>> remove([["record", "one"]], "a")
+    >>> remove([["record", "one"]], -3)
+    >>> remove([["record", "one"]], 1)
+    """
+
+    if type(id_) != int: return
+    if len(table) <= id_ or id_ < 0: return
+    
+    output_table = table.copy()
+    del output_table[id_]
+
+    return output_table
+
+
+def update(table, id_, record):
+    """
+    Updates specified record in the table.
+
+    Args:
+        table: list in which record should be updated
+        id_ (str): id of a record to update
+        record (list): updated record
+
+    Returns:
+        list: table with updated record
+    
+    >>> update([['record', 'one'], ['record', 'two']], 1, ['new', 'record'])
+    [['record', 'one'], ['new', 'record']]
+
+    >>> update([["a", "b"]], 0, [])
+    [[]]
+
+    >>> update([[]], 0, ['new', 'record'])
+    [['new', 'record']]
+
+    >>> update([['old_record']], "a", ['new_record'])
+    [['old_record']]
+
+    >>> update([['old_record']], -3, ['new_record'])
+    [['old_record']]
+    
+    >>> update([['old_record']], 1, ['new_record'])
+    [['old_record']]
+    """
+
+    if type(id_) != int: return table.copy()
+    if len(table) <= id_ or id_ < 0: return table.copy()
+    
+    output_table = table.copy()
+    output_table[id_] = record
+
+    return output_table
+
+
 def generate_random(table):
     """
     Generates random and unique string. Used for id/key generation:
@@ -37,6 +138,4 @@ def generate_random(table):
         try:
             generate_random(table)
         except RecursionError:
-            print("Stack overflow error!")
-
-print(generate_random(["kH94Ju#&"]))
+            print("Stack overflow error!")s
