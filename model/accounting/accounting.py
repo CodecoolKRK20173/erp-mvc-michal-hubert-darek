d13c8b312f6d98249167c0de9d99fd72ab91d714
Dariusz Serwaczak "Innewit <serwaczak.dariusz@gmail.com>
2019-02-27 11:41:52 +0100

    Added which_year_max function with doctest

--- model/accounting/accounting.py
+++ model/accounting/accounting.py
@@ -11,17 +11,14 @@ Data table structure:
 """
 
 # everything you'll need is imported:
-import sys
-sys.path.insert(0, '/home/darek/Desktop/Repositories/erp-mvc-michal-hubert-darek/model')
-
-import data_manager
-import common
+#from model import data_manager
+#from model import common
 
 # special functions:
 # ------------------
-print(data_manager.get_table_from_file("items.csv"))
+
 def which_year_max(table):
-    """
+    '''
     Question: Which year has the highest profit? (profit = in - out)
 
     Args:
@@ -29,9 +26,32 @@ def which_year_max(table):
 
     Returns:
         number
-    """
 
-    # your code
+    >>> which_year_max([["kH14Ju#&", 1, 21, 2015, "out", 100], ["kH38Jm#&", 10, 23, 2016, "out", 100],\
+    ["eH34Jd#&", 2, 12, 2015, "in", 400], ["kH38Ju#&", 3, 10, 2016, "in", 100]])
+    2015
+    '''
+    __YEAR_INDEX = 3
+    __TYPE_INDEX = 4
+    __INCOME_INDEX = 5
+    years = dict.fromkeys([table[index][__YEAR_INDEX] for index in range(len(table))], 0)
+    keys = list(years.keys())
+    for record in table:
+        if record[__TYPE_INDEX] == "out":
+            in_or_out = -1
+        else:
+            in_or_out = 1
+        years[record[3]] += int(record[__INCOME_INDEX]) * in_or_out
+    
+    highest_profit = next(iter(years.values()))
+
+    for value in years.values():
+        if highest_profit < value:
+            highest_profit = value
+
+    for key in years.keys():
+        if highest_profit == years[key]:
+            return key
 
 
 def avg_amount(table, year):
