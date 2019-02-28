c9a990477e604e03313bf8db93f8326eb48c6389
Michal Nabielec <michal.nabie@gmail.com>
2019-03-01 00:38:38 +0100

    Add CRUD in crm module

--- controller/crm_controller.py
+++ controller/crm_controller.py
@@ -3,6 +3,7 @@ from view import terminal_view
 from model.crm import crm
 from controller import common
 
+
 def run():
     """
     Starts this module and displays its menu.
@@ -12,14 +13,29 @@ def run():
     Returns:
         None
     """
-
-    menu = ["Show subscribers", "Show id of person with the longest surname"]
+    menu_sale = ["Print store list", "Add to store list", "Remove form store list", "Update record in store list",
+                 "Show subscribers", "Show longest name ID"]
+    title = ["ID", "Name", "E-mail",  "Subscribed"]
+    title_del = ["Input ID: "]
     choice = None
     while choice != "0":
-        choice = terminal_view.get_choice(menu)
+        choice = terminal_view.get_choice(menu_sale)
+
         if choice == "1":
-            terminal_view.print_result(crm.get_subscribed_emails(crm.get_file()), "Subscribers")
-        elif choice == "2":
-            terminal_view.print_result(crm.get_longest_name_id(crm.get_file()), "Id of person with the longest surname")
-        else:
-            terminal_view.print_error_message("There is no such choice.")
+            terminal_view.print_table(crm.get_data(), title)
+
+        if choice == "2":
+            crm.new_record(common.get_user_inp_record(terminal_view.get_inputs(title[1:], "")),
+                           "model/crm/customers.csv")
+        if choice == "3":
+            crm.delete_record(crm.get_data(),
+                              common.get_user_inp_record(terminal_view.get_inputs(title_del, "")),
+                              "model/crm/customers.csv")
+        if choice == "4":
+            crm.update_record(crm.get_data(),
+                              common.get_user_inp_record(terminal_view.get_inputs(title_del, "")),
+                              terminal_view.get_inputs(title[1:], ""), "model/crm/customers.csv")
+        if choice == "5":
+            terminal_view.print_result(crm.get_subscribed_emails(crm.get_data()), "Subscribers")
+        if choice == "6":
+            terminal_view.print_result(crm.get_longest_name_id(crm.get_data()), "Longest name ID")
